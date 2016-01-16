a1e5ba13875c4f47f859222daff9595753882d8d
Yuta Yamada <cokesboy@gmail.com>
2016-01-15 18:29:21 -0800

    Add template example

---
+++ examples/times.nim
@@ -0,0 +1,11 @@
+# Ruby style `times`
+# http://forum.nim-lang.org/t/1940
+template times(x: expr, body: stmt): stmt =
+  for i in 1..x:
+    body
+
+
+when isMainModule:
+  var num = 0
+  5.times: num += 1
+  assert(5 == num)
